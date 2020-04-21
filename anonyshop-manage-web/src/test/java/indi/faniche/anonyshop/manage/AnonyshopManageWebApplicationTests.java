eb85be52c39271aedebb68d9b699b1b89431e3a9
faniche <guoyanan23215@gmail.com>
2020-04-21 17:41:34 +0800

    Manage web test

---
+++ anonyshop-manage-web/src/test/java/indi/faniche/anonyshop/manage/AnonyshopManageWebApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.faniche.anonyshop.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonyshopManageWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
