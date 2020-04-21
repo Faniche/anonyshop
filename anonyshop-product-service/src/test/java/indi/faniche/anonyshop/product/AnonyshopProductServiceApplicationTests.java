a5a7eb5f3f6f65f03450a6db4d2cfae81b90f3a3
faniche <guoyanan23215@gmail.com>
2020-04-21 17:39:49 +0800

    Product service

---
+++ anonyshop-product-service/src/test/java/indi/faniche/anonyshop/product/AnonyshopProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.faniche.anonyshop.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonyshopProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
